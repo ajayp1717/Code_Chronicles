b7912c60168772081ec4d22a46f7a14020f9809a
Ajay Patil <ajay.pict@gmail.com>
2023-08-02 18:32:58 +0530

    dev done

---
+++ src/pages/about/index.js
@@ -0,0 +1,113 @@
+import React from "react";
+import "./style.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  dataabout,
+  meta,
+  worktimeline,
+  skills,
+ achievements,
+} from "../../content_option";
+
+export const About = () => {
+  return (
+    <HelmetProvider>
+      <Container className="About-header">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title> About | {meta.title}</title>
+          <meta name="description" content={meta.description} />
+        </Helmet>
+        <Row className="mb-5 mt-3 pt-md-3">
+        <Col lg="8">
+  <h1 className="display-4 mb-4">About me</h1>
+  <h5>
+    Final year undergrad at{" "}
+    <a href="https://www.pict.edu/" target="_blank" rel="noopener noreferrer">
+      Pune Institute Of Computer Technology
+    </a>
+    , majoring in Computer Science & Engineering.
+  </h5>
+  <hr className="t_border my-4 ml-0 text-left" />
+</Col>
+        </Row>
+        <Row className="sec_sp">
+          <Col lg="5">
+            <h3 className="color_sec py-4">{dataabout.title}</h3>
+          </Col>
+          <Col lg="7" className="d-flex align-items-center">
+            <div>
+              <p>{dataabout.aboutme}</p>
+            </div>
+          </Col>
+        </Row>
+        <Row className=" sec_sp">
+          <Col lg="5">
+            <h3 className="color_sec py-4">Learning Curve</h3>
+          </Col>
+          <Col lg="7">
+            <table className="table caption-top">
+              <tbody>
+                {worktimeline.map((data, i) => {
+                  return (
+                    <tr key={i}>
+                      <th scope="row">{data.jobtitle}</th>
+                      <td>{data.where}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </Col>
+        </Row>
+        <Row className="sec_sp">
+          <Col lg="5">
+            <h3 className="color_sec py-4">Skills</h3>
+          </Col>
+          <Col lg="7">
+            <table className="table caption-top">
+                <tbody>
+                {skills.map((data, i) => {
+                  return (
+                    <React.Fragment key={i}>
+                      <tr>
+                        <th scope="row">{data.skill}</th>
+                        <td>{data.list}</td>
+                      </tr>
+                    </React.Fragment>
+                  );
+                })}
+              </tbody>
+            </table>
+          </Col>
+        </Row>
+        <Row className="sec_sp">
+          <Col lang="5">
+            <h3 className="color_sec py-4">Achievements</h3>
+          </Col>
+          <Col lg="7">
+            {achievements.map((data, i) => {
+              return (
+                <div className="service_ py-4" key={i}>
+                  <h5 className="service__title">{data.title}</h5>
+                  <p className="service_desc">{data.description}</p>
+                  <div>
+                    {data.links.map((link, index) => (
+                      <React.Fragment key={index}>
+                        <a href={link.url} target="_blank" rel="noopener noreferrer">
+                          {link.name}
+                        </a>{"   "}
+                       
+                      </React.Fragment>
+                    ))}
+                  </div>
+                </div>
+              );
+            })}
+          </Col>
+        </Row>
+      </Container>
+    </HelmetProvider>
+  );
+};
